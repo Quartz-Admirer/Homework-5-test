d163bebbfcb89ed9586a12e9fbe94f70573245ac
Quartz_Admirer <118068910+Quartz-Admirer@users.noreply.github.com>
2024-07-06 23:46:41 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
-// next.config.mjs
 export default {
   reactStrictMode: true,
-  images: {
-    domains: ['yourdomain.com'], // Добавьте домен, если вы используете внешние изображения
-  },
-  trailingSlash: true, // Включение окончания слэша для маршрутов
+  trailingSlash: true,
 };
